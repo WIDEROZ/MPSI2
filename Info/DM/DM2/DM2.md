fce6192c629a7ed5486f8c8298b0f8277486b437
WIDEROZ <roftelio001@gmail.com>
2024-05-09 02:53:02 +0200

    DM Info

---
+++ Info/DM/DM2/DM2.md
@@ -0,0 +1,70 @@
+# Exercice 1
+## Question 1
+Soit $i, j \in [\![0, 3]\!]$ et $k \in [\![1, 4]\!]$
+
+Pour chaque case à la coordonné $(i, j)$ on a $4$ valeurs possibles pour $k$ : $1, 2, 3, 4$
+Si on fixe la ligne $i$ on a donc $4$ possibilités pour choisir la colonne. 
+Alors pour une ligne fixé on à : $4 \times 4 = 16$ possibilités
+Or dans la grille on a : $4$ lignes, on ajoute donc 4 fois chaque nombre de variables possibles dans une ligne 
+Ainsi, on a : 
+$64$ variables propositionnelles
+___
+
+
+Compter le nombre de valuation reviens à compter le nombre de grilles de sudoku différentes qui suivent les règles.
+
+
+On remplit la <u>première ligne</u> du tableau du sudoku,
+- Dans la première case de la ligne on a $\boxed{4}$ possibilités de valuation $k_{1} \in [\![1, 4]\!]$ (car le tableau n'est pas encore rempli) 
+- Dans la deuxième case de la ligne $v(x_{1, 0, k_{1}}) = 0$ (par la règle sur la ligne) alors on prend une valeur $k_{2} \in [\![1, 4]\!] \setminus \{ k_{1} \}$ cela revient à choisir quel élément sera bien évalué à $1$ dans l'ensemble : $\{ x_{0, 0, v} ; v \in [\![1, 4]\!] \setminus \{ k_{1} \} \}$ ce qui nous fait $\boxed{3}$ possibilités de valuation possible. 
+
+Par analogie pour les $2$ dernières cases de la ligne :
+
+$$\text{Ainsi, pour la première ligne on a : } \boxed{4! =24} \text{ choix de valuation}$$
+
+
+On remplit la <u>deuxième ligne</u> : 
+
+- Dans la première case de la ligne on prend une valeur $k_{5} \in [\![1, 4]\!] \setminus \{ k_{1}, k_{2} \}$ (car on doit respecter la règle du carré $2 \times 2$ et de la colonne) on doit alors choisir entre les valuations des éléments de cet ensemble : $\{ x_{1, 0, v} : v \in [\![1, 4]\!] \setminus \{ k_{1}, k_{2} \} \}$ on a alors $\boxed{2}$ choix. 
+- Ce qui nous laisse pas le choix pour le choix de la valeur de la case $(1, 1)$ : $v(x_{1, 1, k_{6}}) = 1$ avec $k_{6} \in [\![1, 4]\!] \setminus \{ k_{1}, k_{2}, k_{5} \}$ ($|[\![1, 4]\!] \setminus \{ k_{1}, k_{2}, k_{5} \}| = 1$)
+
+- Dans la troisième case on choisit une valeur $k_{7} \in [\![1, 4]\!]\setminus \{ k_{5}, k_{6}, k_{3}, k_{4}\}$ or $k_{4} = k_{5}$ ou $k_{4} = k_{6}$ sinon $k_{4}$ serait égal à un élément de sa ligne.
+  On a deux cas :
+  - Si $k_{5} = k_{3}$, alors, comme $k_{3} \neq k_{4}$ ($k_{3}$ et $k_{4}$ sur la même ligne) on a $k_{5}\neq k_{4}$ et $k_{6} \neq k_{5}$ (car $k_{6}$ et $k_{5}$ sont sur la même ligne) alors $|[\![1, 4]\!]\setminus \{ k_{5}, k_{6}, k_{3}, k_{4}\}| = 1$
+  - Si $k_{6} = k_{3}$, par analogie il ne reste qu'une possibilité
+  
+  Dans les deux cas il ne reste donc qu'<u>une possibilité</u>
+
+- Dans la dernière case il ne reste finalement qu'<u>une possibilité</u>
+
+$$\text{Finalement pour la deuxième ligne on a } \boxed{2} \text{ choix de valuation}$$
+
+
+On remplit la <u>troisième ligne</u> : 
+- Pour la première case $(2, 0)$ :
+  on a que $\boxed{2}$ choix disponibles par la règle des colonnes
+- Pour la deuxième case $(2, 1)$ : 
+  comme la valeur de la case $(2, 0)$ est différente de la case $(0, 0)$ et $(1, 0)$ elle est forcément égale a une des valeurs au dessus de la deuxième (par la règle du carré)
+  on a donc $\boxed{2}$ possibilités de remplissage. 
+- Pour les deux dernières cases $(2, 2)$ et $(2, 3)$ :
+  Si la valeur de la case $(2, 0)$ et la valeur de la case $(2, 1)$ auraient été toutes les deux au dessus de la valeur de la case $(2, 2)$ (respectivement $(2, 3)$),
+  comme les valeur de $(0, 2)$ et $(1, 2)$ et $(0, 3)$ et $(1, 3)$ sont différents entre eux par la règle du carré,  les valeurs de $(2, 0)$ et $(2, 1)$ et $(0, 3)$ et $(1, 3)$ (respectivement $(2, 0)$ et $(2, 1)$ et $(0, 2)$ et $(1, 2)$) le sont aussi, alors on aurait pas de possibilité de remplir la case $(2, 3)$ (respectivement $(2, 2)$). 
+  
+  Donc, les valeur des cases $(2, 0)$ et $(2, 1)$ sont chacune au dessus de : soit $(2, 2)$, soit $(2, 3)$.
+  Mais jamais les les valeur des cases $(2, 0)$ et $(2, 1)$ au dessus d'une unique case : soit $(2, 2)$ soit $(2, 3)$
+  
+  Comme on a bien au dessus de $(2, 2)$ et $(2, 3)$ exactement une valeur différente à $(2, 0)$ ou $(2, 1)$, sur la ligne d'indice $2$ et la colonne d'indice $2$ (respectivement $3$) on a exactement $3$ valeurs différentes. 
+  
+  Ainsi, il ne reste plus qu'<u>une seule possibilité</u> pour remplir les deux dernières cases de la ligne.
+
+$$\text{Finalement pour la troisième ligne on a } \boxed{4} \text{ choix de valuation}$$
+
+
+On remplit la <u>quatrième ligne</u> : 
+Pour chaque case on applique la règle des colonnes il ne nous reste donc <u>pas le choix</u> pour le placement des dernières valeurs.
+
+
+Ainsi, on a $24 \times 2 \times 4 \times 1 = 192$ grilles de sudoku différentes donc,
+$$\boxed{192} \text{ valuations possibles}$$
+
+## Question 2
---
+++ Info/DM/DM2/sudoku.ml
@@ -0,0 +1,250 @@
+type formule = Vrai | Faux | Var of int*int*int | Neg of formule
+             | Et of formule list | Ou of formule list ;;
+
+
+
+(* QUESTION 2 *)
+
+(* (a) *)
+
+let rec listEtL i = 
+  let rec listEtC j = 
+    let rec listVal k = 
+      match k with
+      | 1 -> [Var(i, j, k)]
+      | _ -> [Var(i, j, k)] @ listVal (k-1)
+    in 
+    match j with
+    | 0 -> [Ou(listVal 4)]
+    | _ -> [Ou(listVal 4)] @ listEtC (j-1)
+  in
+  match i with
+  | 0 -> listEtC 3
+  | _ -> listEtC 3 @ listEtL (i-1)
+  
+;;
+
+let listEt = listEtL 3;;
+
+let grille_complete = Et(listEt);;
+
+(* (b) *)
+
+let rec listNeg i j k = 
+  let rec aux l = 
+    match l with
+    | 1 -> if(l == k) then
+            [Var(i, j, l)]
+           else
+            [Neg(Var(i, j, l))]
+    | _ -> if(l == k) then
+            [Var(i, j, l)] @ aux (l-1)
+           else
+            [Neg(Var(i, j, l))] @ aux (l-1)
+  in
+    match k with
+    | 1 -> [Et(aux 4)]
+    | _ -> [Et(aux 4)] @ listNeg i j (k-1)
+
+;;
+
+
+let un_par_case = 
+  let rec parcoursL i = 
+    let rec parcoursC j = 
+      match j with
+      | 0 -> [Ou(listNeg i j 4)]
+      | _ -> [Ou(listNeg i j 4)] @ parcoursC (j-1)
+    in
+    match i with
+    | 0 -> parcoursC 3
+    | _ -> parcoursC 3 @ parcoursL (i-1)
+  in
+  Et(parcoursL 3)
+;;
+
+
+(* let un_par_ligne = *)
+
+
+(* let un_par_colonne = *)
+
+
+(* let un_par_carre = *)
+
+
+
+(* QUESTION 5 *)
+
+
+(* let formule_grille t = *)
+  (* Prend en entrée une grille de sudoku sous la forme d'un tableau 4x4
+     et renvoie la conjonction de variables correspondant à l'information
+     des cases remplies
+  *)
+    
+  
+
+
+(* QUESTION 7 *)
+
+let rec substitution_sudoku f v1 v2 v3 b =
+  (* prend en entrée une formule f, trois entiers v1, v2, v3 et un booléen b
+     et renvoie la formule obtenue par substitution de Var(v1,v2,v3) par Vrai
+     si b vaut true et Faux si b vaut false
+  *)
+  let rec subs_list l1 = match l1 with
+    | [] -> []
+    | t::s -> (substitution_sudoku t v1 v2 v3 b)::(subs_list s)
+  in match f with
+  | Vrai -> Vrai
+  | Faux -> Faux
+  | Var(x1,x2,x3) when (x1,x2,x3) = (v1,v2,v3) -> if b then Vrai else Faux
+  | Var(_) -> f
+  | Neg(f1) -> Neg(substitution_sudoku f1 v1 v2 v3 b)
+  | Et(l) -> Et(subs_list l)
+  | Ou(l) -> Ou(subs_list l) ;;
+
+(* let rec simplif_list_et l = *)
+  (* Prend en entrée une liste de formule et supprime les Vrai de la liste *)
+
+(* let rec simplif_list_ou l = *)
+
+(* let rec vrai_in l = *)
+  (* Renvoie true si la formule Vrai est dans l est false sinon *)
+
+(* let rec faux_in l = *)
+  (* Renvoie true si la formule Faux est dans l est false sinon *)
+
+
+let rec simplif_quine_sudoku f =
+  (* Prend en entrée une formule f et la simplifie selon les règles
+  de l'algorithme de Quine *)
+  let rec simplif_quine_list l =
+    match l with
+    |[] -> []
+    |t::s -> (simplif_quine_sudoku t)::(simplif_quine_list s) 
+  in
+  match f with
+  | Faux | Vrai | Var(_,_,_) -> f
+  | Et(l) -> let l2 = simplif_list_et (simplif_quine_list l) in
+      if faux_in l2 then Faux else
+      if l2 = [] then Vrai
+      else Et(l2)
+  | Ou(l) -> let l2 = simplif_list_ou (simplif_quine_list l) in
+      if vrai_in l2 then Vrai else
+      if l2 = [] then Faux 
+      else Ou(l2)
+  | Neg(f) -> let f2 = simplif_quine_sudoku f in
+      if f2 = Faux then Vrai else
+      if f2 = Vrai then Faux else
+        Neg(f2) ;; 
+
+let rec insertion_sans_doublon e l =
+  match l with
+  |[] -> [e]
+  | t::s -> if t = e then l else t::(insertion_sans_doublon e s) ;;
+
+let rec fusion_sans_doublon l1 l2 =
+  match l1 with
+  | [] -> l2
+  | t::s -> insertion_sans_doublon t (fusion_sans_doublon s l2) ;;
+
+let rec variables f =
+  (* Prend en entrée une formule et renvoie la liste de ses variables *)
+  match f with
+  | Vrai | Faux -> []
+  | Var(x1,x2,x3) -> [(x1,x2,x3)]
+  | Neg(f2) -> variables f2
+  | Et(l) -> (match l with
+      | [] -> []
+      | t::s -> fusion_sans_doublon (variables t) (variables (Et(s)) )) 
+  | Ou(l) -> match l with
+    | [] -> []
+    | t::s -> fusion_sans_doublon (variables t) (variables (Ou(s)) ) ;;
+
+
+
+
+type arbre_quine_sudoku = Valide | Invalide | Noeud of int * int * int * arbre_quine_sudoku * arbre_quine_sudoku ;;
+
+let rec arbre f =
+  (* Prend en entrée une formule et renvoie l'arbre de quine associé *)
+  let f_bis = simplif_quine_sudoku f in
+  match f_bis with
+  | Vrai -> Valide
+  | Faux -> Invalide
+  | _ -> let var = variables f_bis in
+      let (x1,x2,x3)::s = var in
+      let f1 = simplif_quine_sudoku (substitution_sudoku f x1 x2 x3 true) in
+      let f2 = simplif_quine_sudoku (substitution_sudoku f x1 x2 x3 false) in
+      Noeud(x1,x2,x3,arbre f1,arbre f2) ;; 
+      
+
+(* QUESTION 8 *)
+
+(* let rec valuation_sat_arbre a = *)
+  (* Prend en entrée un arbre de quine associé à une formule et renvoie
+     un couple (liste, booléen) avec :
+  -si la formule n'est pas satisfiable, la liste vide et le booléen false
+  -si la formule est satisfiable, une liste de variables à satisfaire pour
+  satisfaire la formule, et le booléen true*)
+
+
+(* QUESTION 9 *)
+
+(* let liste_to_val l = *)
+  (* Prend en entrée une liste de variables propositionnelles et
+  renvoie une matrice 4x4 avec à la case (i,j) :
+  -la valeur k si la liste contient une variable x_{i,j,k}
+  -la valeur 0 s'il n'y a pas de variable x_{i,j,k} *)
+
+
+
+(* QUESTION 10 *)
+
+(* let valuation_sat f = *)
+  (* prend en entrée une formule du calcul propositionnel et renvoie
+  la matrice 4x4 en sortie de liste_to_val correspondant à une liste de variables
+  à satisfaire pour satisfaire la formule en entrée *)
+
+
+
+(* QUESTION 11 *)
+
+(* let solution_grille t = *)
+  (* prend en entrée une grille de sudoku sous la forme d'une matrice 4x4 et
+  renvoie la grille remplie *)
+
+
+(* QUESTION 12 *)
+
+(* let fichier_solution nom_fichier t = *)
+  (* prend en entrée un nom de fichier sans extension sous la forme d'une
+  chaîne de caractère, un tableau t correspondant à une grille de sudoku,
+  et stocke dans un fichier [nom_fichier].txt la solution de la grille *)
+
+
+
+
+let t1 = [| [|3;0;2;0|];
+            [|2;4;1;0|];
+            [|0;0;0;0|];
+            [|1;0;0;0|];|]
+
+let t2 = [| [|1;0;3;4|];
+            [|0;4;0;0|];
+            [|0;0;2;0|];
+            [|0;0;0;0|];|]
+
+let t3 = [| [|2;0;0;0|];
+            [|0;1;2;0|];
+            [|3;0;0;1|];
+            [|0;0;0;0|];|]
+
+
+fichier_solution "grille_1" t1 ;;
+
+fichier_solution "grille_2" t2 ;;
+
+fichier_solution "grille_3" t3 ;;
