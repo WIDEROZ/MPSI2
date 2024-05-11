1fe26087e035974be897444ec65c864cf90d8a8f
WIDEROZ <roftelio001@gmail.com>
2024-05-11 11:12:13 +0200

    obsidian-auto-push : 2024-05-11 11:12:13

--- Mathématiques/DM/DM19/DM19.py
+++ Mathématiques/DM/DM19/DM19.py
@@ -1,6 +1,20 @@
 from numpy.random import randint
 def Xr():
+    tmp = randint(1, 3)
+    if tmp == 1 :
+        return 4
+    else :
+        return randint(1, 4)
 
-    return randint(1, 4)
+def Xv():
+    return randint(1, 5)
 
-print(Xr())
+def Xb():
+    tmp = randint(1, 4)
+    return randint(1, 5)
+
+
+def valide(n):
+    frequenceApparition = (0, 0, 0)
+    while n > 0:
+
