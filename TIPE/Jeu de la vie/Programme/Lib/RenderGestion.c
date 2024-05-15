a432eaf38be655284bb8c70328909206091e8f4e
WIDEROZ <roftelio001@gmail.com>
2024-05-15 16:57:38 +0200

    obsidian-auto-push : 2024-05-15 16:57:38

--- TIPE/Jeu de la vie/Programme/Lib/RenderGestion.c
+++ TIPE/Jeu de la vie/Programme/Lib/RenderGestion.c
@@ -31,6 +31,6 @@ void CASE_CLICK_DISPLAY(SDL_Window *window, SDL_Renderer *renderer, SDL_Texture 
 }
 
 void MOVE_CAMERA_MOUSE(SDL_Renderer *renderer, SDL_Texture *texture, SDL_Rect *camera, int xRel, int yRel){
-    camera -> x += xRel;
-    camera -> y += yRel;
+    camera -> x -= xRel;
+    camera -> y -= yRel;
 }
