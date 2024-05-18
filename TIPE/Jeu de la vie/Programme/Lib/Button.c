e1fe0054a9a75b9ff192e062751621757c14f634
WIDEROZ <roftelio001@gmail.com>
2024-05-18 14:02:33 +0200

    obsidian-auto-push : 2024-05-18 14:02:33

--- TIPE/Jeu de la vie/Programme/Lib/Button.c
+++ TIPE/Jeu de la vie/Programme/Lib/Button.c
@@ -1,21 +1,7 @@
-#include <stdlib.h>
 #include "Button.h"
-Button * CREATE_BUTTON(SDL_Texture *texture, int x, int y, int w, int h){
-    Button * button = malloc(sizeof(Button));
-    button->texture = texture;
-
-    SDL_Rect * rect = malloc(sizeof(SDL_Rect));
-    rect->x = x;
-    rect->y = y;
-    rect->w = w;
-    rect->h = h;
-    button->position = rect;
-    return button;
-
-}
 
 Button * CREATE_BUTTON(SDL_Texture *texture, int x, int y, int w, int h, char text){
-    /*Create a button in adding text*/
+    /*Créé un bouton avec du text*/
     Button * button = malloc(sizeof(Button));
     button->texture = texture;
 
@@ -29,4 +15,10 @@ Button * CREATE_BUTTON(SDL_Texture *texture, int x, int y, int w, int h, char te
     button -> text= text;
     return button;
 
+}
+
+bool BUTTON_CLICK(Button * button, SDL_Event * e){
+    if(e->type == SDL_MOUSEBUTTONDOWN && button->position->x <= (e->motion->x) && (e->motion->x) <= ){
+
+    }
 }
--- TIPE/Jeu de la vie/Programme/Lib/Button.h
+++ TIPE/Jeu de la vie/Programme/Lib/Button.h
@@ -1,9 +1,14 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL.h>
 
 typedef struct Button
 {
+    // ATTENTION DONNER LES COORDONNES DU BOUTTON PAR RAPPORT A LA TEXTURE
     SDL_Texture *texture;
     SDL_Rect *position;
     char text;
 } Button;
+
+
+Button * CREATE_BUTTON(SDL_Texture *texture, int x, int y, int w, int h, char text);
