b0eaf95d3ff5300ab9f998c090e21989b638a3e3
WIDEROZ <roftelio001@gmail.com>
2024-05-20 19:39:50 +0200

    obsidian-auto-push : 2024-05-20 19:39:50

--- TIPE/Jeu de la vie/Programme/Lib/Button.c
+++ TIPE/Jeu de la vie/Programme/Lib/Button.c
@@ -5,7 +5,7 @@
     ATTENTION BIEN METTRE LES COORDONÉS DE LA TEXTURE
     
 */
-Button * CREATE_BUTTON(SDL_Renderer * renderer, SDL_Texture *texture, int x, int y, int w, int h, char *text){
+Button * CREATE_BUTTON(SDL_Renderer * renderer, SDL_Texture *texture, int x, int y, int w, int h, char *text, TTF_Font *font){
     
     Button * button = malloc(sizeof(Button));
     button->texture = texture;
@@ -20,15 +20,31 @@ Button * CREATE_BUTTON(SDL_Renderer * renderer, SDL_Texture *texture, int x, int
     button->isClicked = 0;
 
     button -> text= text;
-    
+
     VERIF_SDL_COMMAND(SDL_SetRenderTarget(renderer, texture), "Set render target");
     
     VERIF_SDL_COMMAND(SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE), "SetRenderDrawColor");
     VERIF_SDL_COMMAND(SDL_RenderFillRect(renderer, rect), "Pas réussi a créer le bouton");
-    
+
+    // Text
+    VERIF_SDL_COMMAND(SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE), "SetRenderDrawColor");
+    SDL_Color color;
+    color.r = 255;
+    color.g = 255;
+    color.b = 255;
+    color.a = 255;
+
+
+    SDL_Surface *surfaceText = TTF_RenderText_Solid(font, text, color);
+
+
 
     VERIF_SDL_COMMAND(SDL_SetRenderTarget(renderer, NULL), "Set render target");
-    
+
+
+
+
+    SDL_FreeSurface(surfaceText);
     return button;
 
 }
--- TIPE/Jeu de la vie/Programme/Lib/TTF.h
+++ TIPE/Jeu de la vie/Programme/Lib/TTF.h
@@ -1,2 +1,1 @@
 #include <SDL_ttf.h>
-
