2507ea20fda1e043759ebd5acac3a5e289c146a9
WIDEROZ <roftelio001@gmail.com>
2024-05-22 14:35:32 +0200

    obsidian-auto-push : 2024-05-22 14:35:32

--- Info/TD/TD9/TD9.ml
+++ Info/TD/TD9/TD9.ml
@@ -1,5 +1,11 @@
 let rec division l = 
   match l with
   | [] -> ([], [])
-  | t1::t2::s -> let (l1, l2) in (t1::l1, )
+  | [v] -> ([], [v])
+  | t1::t2::s -> let (l1, l2) = division s in (t1::l1, t2::l2)
 ;;
+
+let rec fusion l1 l2 = 
+  match l1 with
+  | [] -> l2
+  | t::s ->  
