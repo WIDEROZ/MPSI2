282c237841998f79c5f4dad7915106442c3cc602
WIDEROZ <roftelio001@gmail.com>
2024-05-29 13:31:09 +0200

    obsidian-auto-push : 2024-05-29 13:31:09

---
+++ Info/Cours/Chapitre 15/script.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct Graphe
+{
+    int taille;
+    bool **mat;
+
+}mat_graphe;
+
+typedef struct LGraphe
+{
+    int taille;
+    int valeur;
+    liste_graphe next;
+}liste_graphe;
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
