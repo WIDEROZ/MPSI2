c709d3e678080b155076a55334e81455ea518463
WIDEROZ <roftelio001@gmail.com>
2024-06-26 22:19:18 +0200

    obsidian-auto-push : 2024-06-26 22:19:18

--- TIPE/JeuDeLaVie/Programme/Lib/Evolution.c
+++ TIPE/JeuDeLaVie/Programme/Lib/Evolution.c
@@ -1,9 +1,27 @@
 #include "Evolution.h"
 
-void evolution(Var var){
-    for (int i = 0; i < var->XY; i++)
+int nbreCellulesAutour(matrix mat, int i, int j){
+    int nbre = 0;
+    for (int k = -1; k < 2; k++)
     {
-        /* code */
+        
+    }
+    
+    return 5454;
+}
+
+void evolution(Var var){
+    int **mat = var->XY_CASE_MAT.mat;
+    for (int i = 0; i < var->XY_CASE_MAT.width; i++)
+    {
+        for (int j = 0; j < var->XY_CASE_MAT.height; j++)
+        {
+            if (mat[i][j] == 0)
+            {
+                
+            }
+            
+        }
     }
     
 }
